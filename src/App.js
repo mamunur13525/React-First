8711e264b6e9d6c24d761e9ae9a548545cbc5458
mamunur13525 <How would you declare an arrow function with no parameter?>
2020-08-23 16:25:40 +0600

    react firdst

--- src/App.js
+++ src/App.js
@@ -3,24 +3,40 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  var name = {
+    first : "mamun",
+    age : 12,
+    school : "parulia ss high school"
+  }
+  var color = {
+    color: "red",
+    backgroundColor: "yellow",
+    fontWeight: "bold",
+    color:"black"
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <p>HIs name is{" "+name.age}</p>
+        <Person name="rubel hossain" age = "12"></Person>
+        <Person name="alamin hossain" age = "43"></Person>
+        <Person name="mamun hossain" age = "23"></Person>
+       
+       
+     
     </div>
   );
 }
 
+function Person(props){
+    
+
+  return (<div>
+          <h1>HI! There.?</h1>
+         
+          <h1>name is :{props.name}</h1>
+          <h2>His Age is :{props.age}</h2>
+  </div>)
+}
 export default App;
