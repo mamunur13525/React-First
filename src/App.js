40f9450de367a0a943c06d1c51d50b4ead656a15
mamunur13525 <How would you declare an arrow function with no parameter?>
2020-08-24 17:25:56 +0600

    react api done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
@@ -10,6 +10,7 @@ const products = [{name: "Photoshop", price: "$200"},
 
 ]
 
+
   return (
     <div className="App">
       <ul>
@@ -19,8 +20,8 @@ const products = [{name: "Photoshop", price: "$200"},
       <ul>
       {products.map(pro => <Products product={pro.name} price = {pro.price}></Products> )}
       <Counter></Counter>
-      
-
+      <Users></Users>
+    
 
       </ul>
 
@@ -51,10 +52,36 @@ function Products(props){
 }
 
 function Counter(){
+ 
   const [count, setCount] = useState(0)
+
+
   return (
     <div>
       <h1>Count: {count}</h1>
+      <button onClick={()=> setCount(count-1)}>Decrease</button>
+      <button onClick={()=> setCount(count +1 )}>Increase</button>
+    </div>
+  )
+}
+
+
+function Users(){
+
+  const [users, setUsers] = useState([]);
+ useEffect(() => {
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(res => res.json())
+  .then(data => setUsers(data))
+})
+  return (
+    <div>
+      <h2>Dynamic Data:{users.length}</h2>
+      <ul>
+        {
+          users.map(user => <li>{user.name}</li>)
+        }
+      </ul>
     </div>
   )
 }
